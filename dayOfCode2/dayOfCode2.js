5fcae9f8a17c364c6977ba568f039f5a2b4d2412
silvydev <srmesino96@gmail.com>
2024-08-20 21:15:21 -0500

    Ajuste: Se ajusta el ejercicio del segundo dia del SevenDayOfCode a nivel de backend

--- dayOfCode2/dayOfCode2.js
+++ dayOfCode2/dayOfCode2.js
@@ -9,28 +9,25 @@ function mostrarMensaje() {
 
     let respuestaUsuario = 0;
 
-    while (respuestaUsuario === 0 || respuestaUsuario !== 1 || respuestaUsuario !== 2) {
+    while (respuestaUsuario == 0) {
 
-        respuestaUsuario = parseInt(prompt(`¿Te gusta estudiar ${lenguaje}? Responde con el número 1 para SÍ o 2 para NO`));
+        respuestaUsuario = parseInt(prompt(`¿Te gusta estudiar ${lenguaje}? Responde con mmmmel número 1 para SÍ o 2 para NO`));
 
         if (respuestaUsuario === 1) {
 
             alert("¡Muy bien! Sigue estudiando y tendrás mucho éxito.");
-            break;
-
+            
         } else if (respuestaUsuario === 2) {
 
             alert("Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?");
-            break;
-
+            
         } else {
 
+            respuestaUsuario = 0;
             alert("este numero no es valido");
 
         }
 
     }
-
-
 
 }
